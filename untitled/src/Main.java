3b16c86b4a219008228f0607e29c6845830a4066
Rafal Suski <rsuski7@wp.pl>
2023-01-23 11:40:58 +0100

    Trzeci comm

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(metodaA("tt","aa"));
+        System.out.println(metodaB("tt","aa"));
+    }
+
+    public static String metodaA(String kod, String... params) {
+        StringBuilder kodyKosztow = new StringBuilder();
+        for (String param : params) {
+            kodyKosztow.append(param).append(",");
+        }
+        kodyKosztow.append(kod);
+        return kodyKosztow.toString();
+    }
+
+    public static String metodaB(String kod, String... params) {
+        StringBuilder kodyKosztow = new StringBuilder();
+        Arrays.stream(params).forEach((param) -> {
+            kodyKosztow.append(param).append(",");
+        });
+        kodyKosztow.append(kod);
+        return kodyKosztow.toString();
     }
 }
